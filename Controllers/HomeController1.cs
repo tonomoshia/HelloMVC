0a0cc236b6a581c2aeab50c4f1b87cd975a8e8a4
Elizabeth Reiher <tonomoshia@gmail.com>
2019-10-30 14:08:39 -0400

    added a controller

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("Hello World");
+        }
+    }
+}
