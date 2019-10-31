b8c447731779d1a90ffa7f1f15119933f9515cb1
Elizabeth Reiher <tonomoshia@gmail.com>
2019-10-31 14:46:58 -0400

    //customize route so post request goes to same page (Hello), post data to same url

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -7,9 +7,11 @@ namespace HelloMVC.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/Hello/Display'>" +
+            //customize route so post request goes to same page (Hello) post data to same url
+            string html = "<form method='post'>" +
                 "<input type='text' name = 'name'  />" +
                  "<input type='submit' value='Greet me!'  />" +
                 "</form>";
@@ -18,6 +20,9 @@ namespace HelloMVC.Controllers
         }
         // used https://localhost:44350/Hello?name=Lola to test query parameter
 
+        //Hello
+        [Route("/Hello")]
+        [HttpPost]
         public IActionResult Display(string name = "World")
         {
             return Content(string.Format("<h1>Hello {0}</h1>", name), "text/HTML");
